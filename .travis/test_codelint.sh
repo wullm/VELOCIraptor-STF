0c65ad74f11267478f85c01e7aa868e0df35d51a
jesmigel <jcantos@swin.edu.au>
2019-07-29 14:30:19 +1000

    Parametization of test script

--- .travis/test_codelint.sh
+++ .travis/test_codelint.sh
@@ -32,6 +32,8 @@ fail() {
 
 # TODO: Remove conditional execution after tests are established
 # 	End goal scope is to target *.cxx files
+# 	Code linter result should be considered as soft warnings.
+# 	Rules are meant to be a guide (for now) it improve readability
 TEST_BRANCH='feature/format';
 TEST_FILE='src/search.cxx';
 if [ "$TRAVIS_BRANCH" == "$TEST_BRANCH" ]; then
