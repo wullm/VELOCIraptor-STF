d20a0c8584c4a1f6f79aee25568c16c42010bbd2
jesmigel <jcantos@swin.edu.au>
2019-07-29 16:23:23 +1000

    Temporary containment of diff output

--- .travis/test_codelint.sh
+++ .travis/test_codelint.sh
@@ -39,7 +39,8 @@ TEST_BRANCH='feature/format';
 TEST_FILE='src/search.cxx';
 OUT_FILE='./search.clang.cxx';
 
-if [ "$TRAVIS_BRANCH" == "$TEST_BRANCH" ]; then
-    clang-format -assume-filename=.clang-format "$TEST_FILE" >> "$OUT_FILE"
+if [ "$TEST_BRANCH" == "$TEST_BRANCH" ]; then
+	rm -f "$OUT_FILE"
+    clang-format -assume-filename=.clang-format "$TEST_FILE" > "$OUT_FILE"
 	diff -yB --suppress-common-lines "$TEST_FILE" "$OUT_FILE"; exit 0;
 fi
