81267917b66538383d1972e0c3e65028f462e443
jesmigel <jcantos@swin.edu.au>
2019-07-29 14:48:04 +1000

    Restructuring the job build

--- .travis/test_codelint.sh
+++ .travis/test_codelint.sh
@@ -37,6 +37,9 @@ fail() {
 # 	readability amongst developers
 TEST_BRANCH='feature/format';
 TEST_FILE='src/search.cxx';
+OUT_FILE='./search.clang.cxx';
+
 if [ "$TRAVIS_BRANCH" == "$TEST_BRANCH" ]; then
-    clang-format -assume-filename=.clang-format "$TEST_FILE" | diff -yB "$TEST_FILE" -; exit 0
+    clang-format -assume-filename=.clang-format "$TEST_FILE" >> "$OUT_FILE"
+	diff -yB "$TEST_FILE" "$OUT_FILE"; exit 0;
 fi
