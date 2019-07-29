254cd7f5aceb2c586179f6333d7a036a5391c1c1
jesmigel <jcantos@swin.edu.au>
2019-07-29 15:24:53 +1000

    Updated travis logic

--- .travis/test_codelint.sh
+++ .travis/test_codelint.sh
@@ -41,5 +41,5 @@ OUT_FILE='./search.clang.cxx';
 
 if [ "$TRAVIS_BRANCH" == "$TEST_BRANCH" ]; then
     clang-format -assume-filename=.clang-format "$TEST_FILE" >> "$OUT_FILE"
-	diff -yB "$TEST_FILE" "$OUT_FILE"; exit 0;
+	diff -yB --suppress-common-lines "$TEST_FILE" "$OUT_FILE"; exit 0;
 fi
